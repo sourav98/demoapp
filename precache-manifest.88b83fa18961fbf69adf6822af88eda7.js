f94f1b750b980ecc877f8e6ce6c52b5863e6f91b
Sourav Sudhi <sourav.sudhi@gmail.com>
2020-05-28 10:38:33 +0530

    Updates

---
+++ precache-manifest.88b83fa18961fbf69adf6822af88eda7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e50546aec25e239b1f4c3d392898cda0",
+    "url": "/demoapp/index.html"
+  },
+  {
+    "revision": "faca9157c209605a826b",
+    "url": "/demoapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1ebc3830d419c984e246",
+    "url": "/demoapp/static/js/2.8e545a19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/demoapp/static/js/2.8e545a19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faca9157c209605a826b",
+    "url": "/demoapp/static/js/main.7c0f6990.chunk.js"
+  },
+  {
+    "revision": "a1f1e932d7712a17498c",
+    "url": "/demoapp/static/js/runtime-main.deb69264.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demoapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
